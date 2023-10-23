fc52753c77f66d990ee75ab05b8139a769d00e9a
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-23 19:50:00 -0400

    add files

---
+++ codePFE/code_complet/secrets.h
@@ -0,0 +1,8 @@
+//  fichier pour stocker toutes les informations d'identification privées.
+// et les détails de connexion
+
+#define SECRET_SSID "SSID"		
+#define SECRET_PASS "Soffi2019"
+
+#define SECRET_CH_ID 778895		// numéro de chaîne
+#define SECRET_WRITE_APIKEY "EN7DYWPGHX8OWJAQ"   //clé API d'écriture de votre canal
